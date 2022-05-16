3b42235b16aed8b084ff73357190bf04ebc127bf
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2022-05-16 15:19:58 +0200

    Make scientist battle playable

--- src/rooms/vetenskapsperson.ts
+++ src/rooms/vetenskapsperson.ts
@@ -1,6 +1,8 @@
 import { Choice, Consequence, Room } from "../game"
 import AbabouFajt from "./ababou-fajt"
 import { createGameOverRoom } from "../helpers/gameOver"
+import battleChoices, { AttacksBattleState, generatePlayerAttacks } from "../helpers/batteChoices"
+import ÅterTillSittningslokal from "./åter-till-sittningslokal"
 
 const Integrallösning: Room = ({ player }) => {
   const wrongIntegralSolution = (text: string): Choice => {
@@ -75,22 +77,79 @@ const Integrallösning: Room = ({ player }) => {
   }
 }
 
-const VetenskapspersonFajt: Room = function () {
+const AbabouEnd: Room = () => ({
+  text:
+    "Väl utanför berget vänder du dig om och ser hur hela berget kollapsar med ett brak. Dvärgarna är utrotade. Ababaou har dykt upp och går fram till dig med ett belåtet uttryck.\n\n" +
+    "[Ababou]: Du har räddat Amazonerna och utrotat de fruktansvärda dvärgarna och deras fruktansvärda vetenskap. Nu är det dags för dig att resa hem.\n\n",
+  choices: [
+    {
+      text: "Låt Ababou föra dig tillbaks till sittningen",
+      onChoose: () => ({ room: ÅterTillSittningslokal }),
+    },
+  ],
+})
+
+type VetenskapspersonBattleState = AttacksBattleState & {
+  stage: "introduction" | "battle"
+}
+
+const VetenskapspersonBattle: Room<VetenskapspersonBattleState> = function (args) {
+  if (!this.state) {
+    this.state = {
+      opponentHp: 200,
+      opponentName: "Slartibartfast",
+      stage: "introduction",
+    }
+  }
+
+  const state = this.state as VetenskapspersonBattleState
+
+  const introduction = state.stage == "introduction"
+  state.stage = "battle"
+
+  const onDefeat = () => ({
+    text: "Du har tagit så mycket skada att du dör. Slartibartfast har besegrat dig.",
+    room: createGameOverRoom(),
+  })
+
   return {
-    text: "TODO",
-    choices: [],
+    text: introduction
+      ? "Du gör dig redo för din slutgiltiga strid som ska fria hela lagomgård från dvärgharnas illegala påhitt. Slartibartfast stirrar ned dig och väntar på att du ska göra ditt första drag."
+      : "Slartibartfast frustar ursinnigt åt dig. Vad gör du?",
+    choices: battleChoices(
+      args,
+      state,
+      generatePlayerAttacks(
+        args,
+        state,
+        () => ({
+          text:
+            "Plötsligt orkar Slartibartfast inte attackera dig längre. Han sätter sig tungt ner i hörnet av rummet och suckar. Du har besegrat honom, men hur ska du kunna förgöra resten av dvärgarna?\n\n" +
+            "Då hör du Ababous röst i ditt huvud: 'Använd satsen!' och plötsligt kommer du ihåg att du har inte-graalen med dig. Du modifierar den till en integral-sats som visar att dvärgarnas boning måste självförstöras om 90 sekunder givet vissa tekniska antaganden, som du hoppas är orelevanta vid denna praktiska tillämpning.",
+          choices: [
+            {
+              text: "Spring ut från berget samma väg som du kom.",
+              onChoose: () => ({ room: AbabouEnd }),
+            },
+          ],
+        }),
+        [
+          {
+            onChoose: () => {
+              args.player.hp -= 15
+              return {
+                text: "Dvärghen hugger dig med sin hacka. Du tar 15 hp skada.",
+              }
+            },
+            onDefeat,
+          },
+        ]
+      )
+    ),
   }
 }
 
 export const VetenskapspersonA: Room = function ({ player }) {
-  const AbabouEnd: Room = () => ({
-    text:
-      "Väl utanför berget vänder du dig om och ser hur hela berget kollapsar med ett brak. Dvärgarna är utrotade. Ababaou har dykt upp och går fram till dig med ett belåtet uttryck.\n\n" +
-      "[Ababou]: Du har räddat Amazonerna och utrotat de fruktansvärda dvärgarna och deras fruktansvärda vetenskap. Nu är det dags för dig att resa hem.\n\n" +
-      "Plötsligt är du tillbaka i sittningslokalen, nöjd med ditt äventyr. Men dina kamrater på sittningen är mindre nöjda, eftersom du inte varit med och städat...",
-    choices: [],
-  })
-
   return {
     text: "En bit in i berget kommer du in i ett laboratorium, En väggen har massa fula tavlor med orangea löv på sig och en annan har en whiteboardtavla med massa ekvationer, de andra har massa dragskåp och någravanliga skåp. Mitt i allt står en dvärg med vilt långt skägg och hår.",
     choices: [
@@ -104,7 +163,7 @@ export const VetenskapspersonA: Room = function ({ player }) {
             choices: [
               {
                 text: "Säg 'Fuck off' och attackera dvärghen!",
-                onChoose: () => ({ room: VetenskapspersonFajt }),
+                onChoose: () => ({ room: VetenskapspersonBattle }),
               },
               {
                 text: "Be dvärghen berätta mer, för du älskar ju kärnkraft för fan.",
@@ -136,7 +195,7 @@ export const VetenskapspersonA: Room = function ({ player }) {
               }
             : {
                 text: "Du blir upptäckt och måste gå i strid!",
-                room: VetenskapspersonFajt,
+                room: VetenskapspersonBattle,
               },
       },
       {
@@ -151,7 +210,7 @@ export const VetenskapspersonA: Room = function ({ player }) {
                 text:
                   "Vetenskapspersonen avbryter dig och säger att du skrivit fel och att integralen skulle spränga er. Du är inte smart nog att kunna förklara dig ur situationen.\n\n" +
                   "Fan, tänker du, antar att det är dags för strid igen...",
-                room: VetenskapspersonFajt,
+                room: VetenskapspersonBattle,
               },
       },
     ],
@@ -166,7 +225,7 @@ export const VetenskapspersonB: Room = function () {
         text: "Kärnkraft? Nej tack!",
         onChoose: () => ({
           text: "Du ser hur Slartibartfasts ansikte mörknar och han blir fullständigt rasande.",
-          room: VetenskapspersonFajt,
+          room: VetenskapspersonBattle,
         }),
       },
       {
